43d79f28c5c831f83c417866a12039b591ac9e81
mko5118 <minpko@gmail.com>
2019-08-20 01:08:00 -0400

    added columns to main app

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,26 +29,28 @@ class App extends Component {
   
     render() {
       
-        const { itemSelect, catgeorySelect, test } = this.state;
+        const { itemSelect, categorySelect, test } = this.state;
 
     return(
     <div className="App">
             <header className="App-header"> 
                 <h1 className="align-self-center">@</h1>
             
-                <button class="f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib black" href="#0">Films</button>
+                <button class="f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib black m2" href="#0">Films</button>
                 <a class="f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib black" href="#0">People</a>
             </header>
 
-
-
-            <SelectList category={this.categorySelect} />
-            <InfoCard
-                subject={test}
-            />
-
-            <p></p>
+<div className="container">
+    <div className="row">
+        <div className="col-sm-6">
+            <SelectList category={categorySelect} />
+        </div>
+        <div className="col-sm-6"> 
+            <InfoCard subject={test} />
+        </div>
+        </div>
     </div>
+</div>
   );
 }
 }
