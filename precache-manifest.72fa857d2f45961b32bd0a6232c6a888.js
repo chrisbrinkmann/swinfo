03254d706ddb9f808006d21e5faa06060ea22f50
chrisbrinkmann <brinkmcd@gmail.com>
2019-08-22 15:41:14 -0400

    Updates

---
+++ precache-manifest.72fa857d2f45961b32bd0a6232c6a888.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f172495894c1802de2307fa7e5eaf7",
+    "url": "/swinfo/index.html"
+  },
+  {
+    "revision": "910726f2bca062ed2b34",
+    "url": "/swinfo/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4ab3ef1e4b86b60ab283",
+    "url": "/swinfo/static/css/main.4f921ba0.chunk.css"
+  },
+  {
+    "revision": "910726f2bca062ed2b34",
+    "url": "/swinfo/static/js/2.ef7fc4a3.chunk.js"
+  },
+  {
+    "revision": "4ab3ef1e4b86b60ab283",
+    "url": "/swinfo/static/js/main.dccf6abe.chunk.js"
+  },
+  {
+    "revision": "630381b67cc33b046339",
+    "url": "/swinfo/static/js/runtime~main.4edbca43.js"
+  },
+  {
+    "revision": "d5bf04d592823d32aa87b746c3b238b6",
+    "url": "/swinfo/static/media/Orbitron-Regular.d5bf04d5.ttf"
+  },
+  {
+    "revision": "5c345b90c45c6fad10386d96cc93a030",
+    "url": "/swinfo/static/media/Starjedi.5c345b90.woff"
+  }
+]);
