1231cc86a29524c26881269afe7d5549a4d22b29
chrisbrinkmann <brinkmcd@gmail.com>
2019-08-24 19:36:52 -0400

    styling and indentation fixes

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,77 +3,69 @@ import Header from './Header'
 import Main from './Main'
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            itemList: [],
-            selectedItem: {},
-            isLoading: false
-        }
-
-        this.handleCategorySelect = this.handleCategorySelect.bind(this)
-        this.handleItemSelect = this.handleItemSelect.bind(this)
+  constructor() {
+    super()
+    this.state = {
+      itemList: [],
+      selectedItem: {},
+      isLoading: false
     }
+
+    this.handleCategorySelect = this.handleCategorySelect.bind(this)
+    this.handleItemSelect = this.handleItemSelect.bind(this)
+  }
 
     // onClick handler called from Button.js
-    async handleCategorySelect(url) {
-        this.setState({ isLoading: true })
-        this.setState({ selectedItem: {} })
+  async handleCategorySelect(url) {
+    this.setState({ isLoading: true })
+    this.setState({ selectedItem: {} })
         
-        try {
-            let dataList = []
+    try {
+      let dataList = []
 
-            // cache the first page of data from the API
-            let resp = await fetch(url)
-            let data = await resp.json()
-            dataList = dataList.concat(data.results)
+      // cache the first page of data from the API
+      let resp = await fetch(url)
+      let data = await resp.json()
+      dataList = dataList.concat(data.results)
 
-            // cache the rest of the pages if there are more
-            while (data.next !== null) {
-                resp = await fetch(data.next)
-                data = await resp.json()
-                dataList = dataList.concat(data.results)
-            }
+      // cache the rest of the pages if there are more
+      while (data.next !== null) {
+      resp = await fetch(data.next)
+      data = await resp.json()
+      dataList = dataList.concat(data.results)
+      }  
 
-            this.setState({ itemList: dataList })
+      this.setState({ itemList: dataList })
             
-        } catch (err) {
-            console.log('Big error', err)
-        }
+      } catch (err) {
+        console.log('Big error', err)
+      }
 
-        this.setState({ isLoading: false })
-    }
+      this.setState({ isLoading: false })
+  }
 
-    // onClick handler called from ItemList.js
-    handleItemSelect = item => {
-        this.setState({ selectedItem: item })
-    }
+  // onClick handler called from ItemList.js
+  handleItemSelect = item => {
+    this.setState({ selectedItem: item })
+  }
 
-    async componentDidUpdate() {
-        console.log(this.state.itemList)
-        console.log(this.state.selectedItem)
-    }
+  render() {
 
-    render() {
+    const { itemList, selectedItem, isLoading } = this.state
 
-        const { itemList, selectedItem, isLoading } = this.state
+    return (
+      <div>
+        <Header handleCategorySelect={this.handleCategorySelect}/>
 
-        return (
-            <div>
-                <Header handleCategorySelect={this.handleCategorySelect}/>
-
-                <Main
-                    itemList={itemList}
-
-                    isLoading={isLoading}
-                    
-                    selectedItem={selectedItem}
-
-                    handleItemSelect={this.handleItemSelect}
-                />
-            </div>
-        );
-    }
+        <Main
+          itemList={itemList}
+          isLoading={isLoading} 
+          selectedItem={selectedItem}
+          handleItemSelect={this.handleItemSelect}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,27 +3,22 @@ import ItemList from '../components/ItemList';
 import InfoCard from '../components/InfoCard';
 
 const Main = ({ itemList, selectedItem, handleItemSelect, isLoading }) => {
-    
-    
-    
-    return (
-        <main className="container-fluid row">
-           
-                <div className="col-md-6">
-                    <ItemList
-                        itemList={itemList}
-                        handleItemSelect={handleItemSelect}
-                        isLoading={isLoading}
-                    />
-                </div>
-                <div className="col-md-6">
-                    <InfoCard
-                        selectedItem={selectedItem}
-                    />
-                </div>
-            
-        </main>
-    )
+  return (
+    <main className="container-fluid row">
+      <div className="col-md-6">
+        <ItemList
+          itemList={itemList}
+          handleItemSelect={handleItemSelect}
+          isLoading={isLoading}
+        />
+      </div>
+      <div className="col-md-6">
+        <InfoCard
+          selectedItem={selectedItem}
+        />
+      </div>
+    </main>
+  )
 }
 
 export default Main
